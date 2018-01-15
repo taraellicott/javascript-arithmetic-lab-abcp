a158fb3dc6b1059bb7c713f1be52d9a045a020ef
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 18:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,16 @@
    return a * b;
  }
  
- function divide() {
+ function divide(a, b) {
    return a / b;
  }
  
  function inc(n) {
-   
+   return n+=;
  } 
  
  function dec(n) {
-   
+   return n-=;
  }
  
  function makeInt(n) {

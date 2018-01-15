4e844b79204fcd42b81e3a776a0e12bc63f68b9d
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 18:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
- inc(n) and dec(n).
+ function add(a, b) {
+   return a + b;
+ }
  
- makeInt(n) and preserveDecimal(n).
+ function subtract(a, b) {
+   return a - b;
+ }
+ 
+ function multiply() {
+   return a * b;
+ }
+ 
+ function divide() {
+   return a / b;
+ }
+ 
+ function inc(n) {
+   
+ } 
+ 
+ function dec(n) {
+   
+ }
+ 
+ function makeInt(n) {
+   
+ }
+ 
+ function preserveDecimal(n) {
+   
+ }
